4b1064b5a28bbf023c739aa63f916050cdaf6609
Mikko Kemppi <mikko.kemppi@tahko.com>
2020-09-04 13:45:02 +0300

    apufunktioita ja yksikkötestejä, step3

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -4,6 +4,15 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, b) => sum + b.likes,0)
 
+const favoriteBlog = (blogs) => {
+  const bestBlog = blogs.reduce((best, b) => {
+    //console.log('tykkääjät',b.likes,best)
+    return (b.likes > best) ? b.likes : best
+  },0)
+  //console.log('mostlikes',bestBlog)
+  return blogs.find((b) => b.likes === bestBlog)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
